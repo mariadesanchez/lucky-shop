f6033e69d3c179751d8d95497afd077060cf3ad8
mariadesanchez <calder07@gmail.com>
2024-03-07 16:34:19 -0300

    MP produccion

--- src/actions/payments/mercado-pago-payment.ts
+++ src/actions/payments/mercado-pago-payment.ts
@@ -3,47 +3,41 @@
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { redirect } from 'next/navigation';
 
-
-
-interface Order{
-        id: string;
-        total: number;
-       
-     
-    
+interface Order {
+  id: string;
+  total: number;
 }
 
+export const mercadoPagoCheckPayment = async (order: Order) => {
+  const client = new MercadoPagoConfig({ accessToken: process.env.NEXT_MERCADO_PAGO_ACCESS_TOKEN! });
 
-export const mercadoPagoCheckPayment = async (order:Order) => {
+  // Cambiar a producción
+  client.sandboxMode(false);
 
-    const client = new MercadoPagoConfig({ accessToken: process.env.NEXT_MERCADO_PAGO_ACCESS_TOKEN! });
-    
+  const preference = new Preference(client);
 
-    const preference = new Preference(client);
+  const res = await preference.create({
+    body: {
+      external_reference: order?.id,
+      items: [
+        {
+          id: order?.id,
+          title: `Order #${order?.id.split("-").at(-1)}`,
+          quantity: 1,
+          unit_price: order.total,
+        },
+      ],
+      redirect_urls: {
+        failure: `${process.env.NEXT_URL}`,
+        success: `${process.env.NEXT_URL}`,
+      },
+      back_urls: {
+        failure: `${process.env.NEXT_URL}`,
+        success: `${process.env.NEXT_URL}`,
+      },
+      auto_return: 'approved',
+    },
+  });
 
-    const res = await preference.create({
-        body: {
-            external_reference: order?.id,
-            items: [
-                {
-                    id: order?.id,
-                    title:  `Order #${order?.id.split("-").at(-1)}`,
-                    quantity: 1,
-                    unit_price: order.total,
-                }
-            ],
-            redirect_urls: {
-                failure: `${process.env.NEXT_URL}`,
-                success: `${process.env.NEXT_URL}`,
-            },
-            back_urls:{
-                failure: `${process.env.NEXT_URL}`,
-                success: `${process.env.NEXT_URL}`,
-            },
-            auto_return: 'approved'
-         }
-        })
-
-    redirect(res.sandbox_init_point!);
-    
-}
+  redirect(res.init_point!); // Usar init_point para el entorno de producción
+};
