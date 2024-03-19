dbe9722c91ca3079600ab8238c3985d834f59c0e
Lucky <calder07@gmail.com>
2024-03-18 23:01:40 -0300

    logout

--- src/actions/auth/logout.ts
+++ src/actions/auth/logout.ts
@@ -1,13 +1,13 @@
 'use server';
 
 import { signOut } from '@/auth.config';
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 
 
 export const logout = async() => {
 
-  // await signOut();
-  redirect("/auth/login");
+  await signOut();
+  // redirect("/auth/login");
 
 
 }
