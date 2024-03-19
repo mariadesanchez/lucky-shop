00bf81bd6728addc346af472a430a94514a150e8
Lucky <calder07@gmail.com>
2024-03-19 17:27:02 -0300

    button salir

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -93,7 +93,7 @@ export const Sidebar = () => {
           </>
         )}
 
-        {/* {isAuthenticated && (
+        {isAuthenticated && (
           <button
             className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
             onClick={() => logout()}
@@ -102,18 +102,8 @@ export const Sidebar = () => {
             <span className="ml-3 text-xl">Salir</span>
           </button>
        
-        )} */}
-
-        {isAuthenticated && (
-          <Link
-            href="/"
-            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-            onClick={() => closeMenu()}
-          >
-            <IoLogInOutline size={30} />
-            <span className="ml-3 text-xl">Salir</span>
-          </Link>
         )}
+        
 
         {!isAuthenticated && (
           <Link
