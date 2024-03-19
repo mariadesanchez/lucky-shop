b06af03207edc5b006ae95f24be11a9a01a6dee3
Lucky <calder07@gmail.com>
2024-03-19 00:40:31 -0300

    update prisma

--- src/actions/payments/mercado-pago-payment.ts
+++ src/actions/payments/mercado-pago-payment.ts
@@ -1,62 +1,54 @@
 'use server';
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { redirect } from 'next/navigation';
-import { revalidatePath } from 'next/cache';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 interface Order {
   id: string;
   total: number;
 }
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 export const mercadoPagoCheckPayment = async (order: Order) => {
-  try {
-    const accessToken = process.env.NEXT_MERCADO_PAGO_ACCESS_TOKEN!;
-    
-    const client = new MercadoPagoConfig({ accessToken });
-  
-    const preference = new Preference(client);
-  
-    const res = await preference.create({
-      body: {
-        external_reference: order?.id,
-        items: [
-          {
-            id: order?.id,
-            title: `Order #${order?.id.split("-").at(-1)}`,
-            quantity: 1,
-            unit_price: order.total,
-          },
-        ],
-        redirect_urls: {
-          failure: `https://lucky-shop-next14.vercel.app/orders/${ order.id }`,
-          success: `https://lucky-shop-next14.vercel.app/orders/${ order.id }`,
-        },
-        back_urls: {
-          failure: `https://lucky-shop-next14.vercel.app/orders/${ order.id }`,
-          success: `https://lucky-shop-next14.vercel.app/orders/${ order.id }`,
-        },
-        auto_return: 'approved',
-      },
-    });
+  const accessToken = process.env.NEXT_MERCADO_PAGO_ACCESS_TOKEN!;
 
-    // Actualizar la base de datos
-    await prisma.order.update({
-      where: { id: order.id },
-      data: {
-        isPaid: true,
-        paidAt: new Date()
-      }
-    });
-  // TODO: Revalidar un path
-  revalidatePath(`/orders/${ order.id }`);
-    // Redirigir al usuario
-    redirect(res.init_point!);
-  } catch (error) {
-    // Manejar cualquier error que ocurra durante el proceso
-    console.error('Error en el proceso de pago:', error);
-    // Podrías lanzar un error o manejarlo de alguna otra forma, dependiendo de tu lógica de aplicación
-  }
+  const client = new MercadoPagoConfig({ accessToken });
+
+  const preference = new Preference(client);
+
+  const res = await preference.create({
+    body: {
+      external_reference: order?.id,
+      items: [
+        {
+          id: order?.id,
+          title: `Order #${order?.id.split("-").at(-1)}`,
+          quantity: 1,
+          unit_price: order.total,
+        },
+      ],
+      redirect_urls: {
+        failure: `https://lucky-shop-next14.vercel.app/orders/${order.id}`,
+        success: `https://lucky-shop-next14.vercel.app/orders/${order.id}`,
+      },
+      back_urls: {
+        failure: `https://lucky-shop-next14.vercel.app/orders/${order.id}`,
+        success: `https://lucky-shop-next14.vercel.app/orders/${order.id}`,
+      },
+      auto_return: 'approved',
+    },
+  });
+
+  // Actualiza la base de datos marcando la orden como pagada
+  await prisma.order.update({
+    where: { id: order.id },
+    data: {
+      isPaid: true,
+      paidAt: new Date(),
+    },
+  });
+
+  // Redirige a la ruta especificada
+  redirect(`/orders/${order.id}`);
 };
